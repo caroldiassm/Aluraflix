b3bf4f9fca22b199852d73639393f25a912a1c13
Carol Dias <caroldias.sm@gmail.com>
2021-09-09 15:45:54 -0300

    alterando o data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,15 @@
 INSERT INTO CATEGORIAS(titulo,cor) VALUES ('Romance','vermelho');
 INSERT INTO CATEGORIAS(titulo,cor) VALUES ('Suspense','azul');
 INSERT INTO CATEGORIAS(titulo,cor) VALUES ('Comédia','amarelo');
-INSERT INTO CATEGORIAS(titulo,cor) VALUES ('Ação','verde');
+INSERT INTO CATEGORIAS(titulo,cor) VALUES ('Drama','verde');
 
-
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('A origem','descricao filme 1','link1.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('A chegada','descricao filme 2','link2.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Os penetras','descricao filme 3','link3.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Star Wars III','descricao filme 4','link4.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Star Wars IV','descricao filme 5','link5.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Star Wars V','descricao filme 6','link6.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('O Poderoso Chefao','descricao filme 7','link7.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Teste 8','descricao filme 8','link8.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Teste 9','descricao filme 9','link9.com.br');
-INSERT INTO VIDEOS(titulo,descricao, url) VALUES ('Testa 10','descricao filme 10','link10.com.br');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('A origem','descricao filme 1','link1.com.br', '2');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('A chegada','descricao filme 2','link2.com.br','2');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Os penetras','descricao filme 3','link3.com.br','3');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Star Wars III','descricao filme 4','link4.com.br','4');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Star Wars IV','descricao filme 5','link5.com.br','4');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Star Wars V','descricao filme 6','link6.com.br','4');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('O Poderoso Chefao','descricao filme 7','link7.com.br','2');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Teste 8','descricao filme 8','link8.com.br','1');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Teste 9','descricao filme 9','link9.com.br','1');
+INSERT INTO VIDEOS(titulo,descricao, url, id_categoria) VALUES ('Testa 10','descricao filme 10','link10.com.br','3');
